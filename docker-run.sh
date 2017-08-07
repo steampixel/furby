3f14793edab4a7a849414d4bf4d9368e0d13bbb2
steampixel <info@steampixel.de>
2017-08-07 16:40:27 +0000

    Auto restart container

--- docker-run.sh
+++ docker-run.sh
@@ -4,21 +4,12 @@
 # run this container as privileged container. This is required for gpio
 # /device/snd is required for sound
 
-#sudo docker run -d -p 80:80 \
-#--name officefurby \
-#--device /dev/mem \
-#-v /dev/snd:/dev/snd \
-#--privileged \
-#steampixel/officefurby
-
-#sudo docker run -d -p 80:80 \
-#--name officefurby \
-#--device /dev/snd \
-#--privileged \
-#steampixel/officefurby
+#--device /dev/mem \ #This is perhaps needed for gpio pins
+#--device /dev/snd \ #This is perhaps needed for sound
 
 sudo docker run -d -p 80:80 \
 --name furby \
 --privileged \
+--restart unless-stopped \
 steampixel/furby
 
